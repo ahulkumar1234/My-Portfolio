6b4c8c6ac73c4cbf3cc2c8b735881451509869c8
🚀 Rahul Kumar | Full Stack Developer <rahulkumar8340527941@gmail.com>
2025-07-03 04:45:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// const roles = ["Web Developer", "Designer", "Freelancer", "Content Creator"];
+// let index = 0;
+
+// function rotateText() {
+//   document.getElementById("autoText").innerText = roles[index];
+//   index = (index + 1) % roles.length;
+// }
+
+// // Change every 2 seconds
+// setInterval(rotateText, 2000);
+
+// // Start immediately
+// rotateText();
