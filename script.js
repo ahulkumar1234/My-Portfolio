bc79cfddbc9006e77a19a3438b8befa6786c294a
🚀 Rahul Kumar | Full Stack Developer <rahulkumar8340527941@gmail.com>
2025-07-06 20:15:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ AOS.init({
   once: true,          // Animate only once while scrolling
   offset: 120,         // Offset (in px) from the original trigger point
 });
+
+
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop default form action
+
+  const formData = new FormData(form);
+
+  fetch("https://formsubmit.co/rahulkumar8340527941@gmail.com", {
+    method: "POST",
+    body: formData
+  })
+    .then(response => {
+      if (response.ok) {
+        alert("✅ Message sent successfully!");
+        form.reset();
+      } else {
+        alert("❌ Failed to send message.");
+      }
+    })
+    .catch(error => {
+      alert("⚠️ Error occurred. Try again later.");
+    });
+});
+
