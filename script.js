824ce86bafe9e87f85c1134f85dc6bc1d41dd3bb
🚀 Rahul Kumar | Full Stack Developer <rahulkumar8340527941@gmail.com>
2025-07-14 00:38:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,65 @@ AOS.init({
 
 
 
-const form = document.getElementById("contact-form");
+// const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); // stop default form action
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault(); // stop default form action
 
-  const formData = new FormData(form);
+//   const formData = new FormData(form);
 
-  fetch("https://formsubmit.co/rahulkumar8340527941@gmail.com", {
-    method: "POST",
-    body: formData
-  })
-    .then(response => {
-      if (response.ok) {
-        alert("✅ Message sent successfully!");
-        form.reset();
-      } else {
-        alert("❌ Failed to send message.");
-      }
-    })
-    .catch(error => {
-      alert("⚠️ Error occurred. Try again later.");
-    });
+//   fetch("https://formsubmit.co/rahulkumar8340527941@gmail.com", {
+//     method: "POST",
+//     body: formData
+//   })
+//     .then(response => {
+//       if (response.ok) {
+//         alert("✅ Message sent successfully!");
+//         form.reset();
+//       } else {
+//         alert("❌ Failed to send message.");
+//       }
+//     })
+//     .catch(error => {
+//       alert("⚠️ Error occurred. Try again later.");
+//     });
+// });
+
+let form = document.querySelector("form");
+let email = document.querySelector("#email");
+let message = document.querySelector("#message");
+
+form.addEventListener("submit", function (dets) {
+  dets.preventDefault();
+
+  document.querySelector("#emailerror").textContent = ""
+  document.querySelector("#textmsg").textContent = ""
+
+
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  let messageRegex = /^[a-zA-Z0-9\s.,!?'"()\-]{10,500}$/
+
+  let emailans = emailRegex.test(email.value);
+  let messageans = messageRegex.test(message.value);
+
+  let isvalid = true
+
+  if (!emailans) {
+    document.querySelector("#emailerror").textContent = "Email is incorrect"
+    emailerror.style.color = "red"
+    isvalid = false
+  }
+  if (!messageans) {
+    document.querySelector("#textmsg").textContent = "Write somthing"
+    textmsg.style.color = "red";
+    isvalid = false
+  }
+
+  if (isvalid) {
+    document.querySelector("#successmessage").textContent = "Thank you message sent successfully!"
+  }
 });
+
 
 
 let mode = document.querySelector(".modebtn");
